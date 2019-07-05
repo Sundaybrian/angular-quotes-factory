a253364a8dfbf7d039f2c1e8b0ef9b3ba3e75a4e
Brian <brians931@gmail.com>
2019-07-05 14:23:31 +0300

    use interpolation to display the user who submitted a quote

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Quotes } from "../../models/quotes";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-quote',
-  templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  selector: "app-quote",
+  templateUrl: "./quote.component.html",
+  styleUrls: ["./quote.component.css"]
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quotes[] = [
+    new Quotes(
+      0,
+      "Stephen Crane",
+      "A man said to the universe Sir, I exist!However,replied the universe The fact has not created in me A sense of obligation.",
+      new Date(1897, 6, 4),
+      "Sunday Brian"
+    ),
+    new Quotes(
+      0,
+      "Stephen Crane",
+      "A man said to the universe Sir, I exist!However,replied the universe The fact has not created in me A sense of obligation.",
+      new Date(1897, 6, 4),
+      "Sunday Brian"
+    ),
+    new Quotes(
+      0,
+      "Stephen Crane",
+      "A man said to the universe Sir, I exist!However,replied the universe The fact has not created in me A sense of obligation.",
+      new Date(1897, 6, 4),
+      "Sunday Brian"
+    ),
+    new Quotes(
+      0,
+      "Stephen Crane",
+      "A man said to the universe Sir, I exist!However,replied the universe The fact has not created in me A sense of obligation.",
+      new Date(1897, 6, 4),
+      "Sunday Brian"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
