79cd64bd7107279d8b98ae89f4f3c4c48756c3b6
Brian <brians931@gmail.com>
2019-07-05 16:17:43 +0300

    add an click expression to hide the quote submitting form on click

--- src/app/components/quotes-form/quotes-form.component.ts
+++ src/app/components/quotes-form/quotes-form.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-quotes-form',
-  templateUrl: './quotes-form.component.html',
-  styleUrls: ['./quotes-form.component.css']
+  selector: "app-quotes-form",
+  templateUrl: "./quotes-form.component.html",
+  styleUrls: ["./quotes-form.component.css"]
 })
 export class QuotesFormComponent implements OnInit {
+  showForm: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  // showQuoteForm() {
 
+  // }
+
+  ngOnInit() {}
 }
