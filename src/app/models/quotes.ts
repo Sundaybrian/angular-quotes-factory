4bc5dbbfc00a9e55d63b128b055b907337db29b4
Brian <brians931@gmail.com>
2019-07-05 13:58:13 +0300

    created the qoutes model

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -1,2 +1,16 @@
 export class Quotes {
+  showDescription: boolean;
+  upvote: number;
+  downvote: number;
+  constructor(
+    public id: number,
+    public author: string,
+    public qoute: string,
+    public date: Date,
+    public user: string
+  ) {
+    this.showDescription = false;
+    this.upvote = 0;
+    this.downvote = 0;
+  }
 }
