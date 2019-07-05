9abe2359b40164fdf8bae4d75ed450c21151cb03
Brian <brians931@gmail.com>
2019-07-05 12:34:21 +0300

    setup initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { QuotesFormComponent } from './components/quotes-form/quotes-form.component';
+import { QuotesDetailsComponent } from './components/quotes-details/quotes-details.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuotesFormComponent,
+    QuotesDetailsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
