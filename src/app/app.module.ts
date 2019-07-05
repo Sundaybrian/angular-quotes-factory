8d828bcc219c14a0296a11663d86d890916b5da2
Brian <brians931@gmail.com>
2019-07-05 13:01:23 +0300

    create the forms markup in the form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './components/quote/quote.component';
@@ -16,7 +17,8 @@ import { NavComponent } from './components/nav/nav.component';
     NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
